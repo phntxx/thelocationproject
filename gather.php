d590c05561f0e5ffcf5ecfcc4102b28a3ebaa4ad
phntxx <meissner.bastian@gmail.com>
2017-05-10 18:39:58 +0200

    THE GEOLOCATION POSTING SYSTEM WORKS.

--- gather.php
+++ gather.php
@@ -1,25 +1,20 @@
 <?php
   session_start();
 
-  $latitude = $_POST["lat"];
-  $longitude = $_POST["lng"];
+  $latitude = $_GET["lat"];
+  $longitude = $_GET["lng"];
   $author = $_SESSION["AUTHOR"];
   $text = $_SESSION["TEXT"];
   $headline = $_SESSION["HEADLINE"];
 
-  echo $latitude;
-  echo $longitude;
-  echo $author;
-  echo $text;
-  echo $headline;
-
-  /*
-  $pdo = new PDO('mysql:host=localhost;dbname=login', 'root', 'raspberry');
+  $pdo = new PDO('mysql:host=localhost;dbname=data', 'root', 'raspberry');
   $statement = $pdo->prepare("INSERT INTO newsfeed (headline, text, author, latitude, longitude) VALUES (:headline, :text, :author, :latitude, :longitude)");
-  $result = $statement->execute(array('headline' => $headline, 'text' => $text, 'author' => $author, 'latitude' => $location, 'longitude' => $location));
+  $result = $statement->execute(array('headline' => $headline, 'text' => $text, 'author' => $author, 'latitude' => $latitude, 'longitude' => $longitude));
   if($result) {
+   $_SESSION["AUTHOR"] = NULL;
+   $_SESSION["TEXT"] = NULL;
+   $_SESSION["HEADLINE"] = NULL;
     header("Location: success.php");
     die();
   }
-  */
 ?>
