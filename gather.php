5675c4c7da3172c584115bb9cafc3c399f2e35f4
phntxx <meissner.bastian@gmail.com>
2017-05-10 18:43:55 +0200

    Added more geolocation stuff
    created gather.php
    edited success.php

---
+++ gather.php
@@ -0,0 +1,25 @@
+<?php
+  session_start();
+
+  $latitude = $_GET["lat"];
+  $longitude = $_GET["lng"];
+  $author = $_SESSION["AUTHOR"];
+  $text = $_SESSION["TEXT"];
+  $headline = $_SESSION["HEADLINE"];
+
+  echo $latitude;
+  echo $longitude;
+  echo $author;
+  echo $text;
+  echo $headline;
+
+  /*
+  $pdo = new PDO('mysql:host=localhost;dbname=login', 'root', 'raspberry');
+  $statement = $pdo->prepare("INSERT INTO newsfeed (headline, text, author, latitude, longitude) VALUES (:headline, :text, :author, :latitude, :longitude)");
+  $result = $statement->execute(array('headline' => $headline, 'text' => $text, 'author' => $author, 'latitude' => $location, 'longitude' => $location));
+  if($result) {
+    header("Location: success.php");
+    die();
+  }
+  */
+?>
